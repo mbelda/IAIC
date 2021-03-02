36f2b2d0955e164af218ffa6c8f03ea2876943a6
Maria José Belda <majo.belda15@gmail.com>
2021-03-02 12:32:13 +0100

    p1 acabada (por revisar)

--- Práctica 1/p1_2.py
+++ Práctica 1/p1_2.py
@@ -10,13 +10,14 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
+feet = 1650
+nHab = 3
 
-
-def normalizar(X, n):
+def normalizar(X):
     mu = []
     sigma = []
     #Para cada atributo (= columna)
-    for i in range(n):
+    for i in range(np.shape(X)[1]):
         #Calculo su media
         mu.append(np.mean(X[:, i]))
         sigma.append(np.std(X[:, i]))
@@ -35,7 +36,66 @@ def calculaGradiente(X, Y, Theta, m):
 def actualizaTheta(X, Y, Theta, m, alpha):
     return Theta - alpha*calculaGradiente(X, Y, Theta, m)
 
+def evolucionCostesModificandoAlpha(X, Y, m, n):
+    #Normalizo X
+    X, mu, sigma = normalizar(X)
+    
+    #Añado una columna de 1s a X
+    X = np.hstack([np.ones([m,1]), X])
+    
+    #Valroes de prueba de alpha
+    alphas = [1.5, 1.0, 0.5, 0.3, 0.1, 0.03, 0.01, 0.001, 0.0001]
+    
+    #Valores fijos
+    nIt = 800  
+    
+    for alpha in alphas:
+        Theta = np.zeros(n+1)
+        costes = []
+        for i in range(nIt):
+            costes.append(coste(X, Y, Theta, m))
+            Theta = actualizaTheta(X, Y, Theta, m, alpha)
+        
+        if alpha == 0.01:
+            #Nos guardamos el theta optimo de un buen alpha
+            Theta_opt = Theta
+        fig = plt.figure()
+        plt.scatter(range(nIt), costes, c='blue')
+        plt.xlabel("Número de iteraciones")
+        plt.ylabel("Coste")
+        plt.legend()
+        plt.title("Evolución del coste con alpha=" + str(alpha))
+        #plt.savefig("regresion_lineal_varias_variables_alpha_" + str(alpha) + ".png")
+    
+    
+    print("-------------- DESCENSO DE GRADIENTE --------------")
+    print("Theta optimo: " + str(Theta_opt))
+    
+    x = np.array([[feet, nHab]])
+    #Normalizo x
+    x = np.divide(np.subtract(x, mu), sigma)
+    x = np.insert(x, 0, 1)
+    
+    pred = np.dot(x, Theta_opt)
+    
+    print("Vivienda de " + str(feet) + " pies cuadrados y " + str(nHab) + " habitaciones")
+    print("Predicción del precio: "+ str(int(pred)) + "$")
+    
+def ecuacionNormal(X, Y, m, n):
+    #Añado una columna de 1s a X
+    X = np.hstack([np.ones([m,1]), X])
+    
+    #Calculamos el theta optimo
+    Theta = np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(X), X)), np.transpose(X)), Y)
 
+    print("-------------- ECUACION NORMAL --------------")
+    print("Theta optimo: " + str(Theta))
+    #Predecimos el precio de una casa con 1650 pies cuadrados y 3 habitaciones
+    x = [1, feet, nHab]
+    pred = np.dot(x, Theta)
+    print("Vivienda de " + str(feet) + " pies cuadrados y " + str(nHab) + " habitaciones")
+    print("Predicción del precio: "+ str(int(pred)) + "$")
+    
 def main():
     #Cargamos los valores de training
     datos = read_csv("ex1data2.csv", header=None).to_numpy().astype(float)
@@ -45,35 +105,11 @@ def main():
     
     m, n = np.shape(X)
     
-    #Normalizo X
-    X, mu, sigma = normalizar(X, n)
-    
-    #Añado una columna de 1s a X
-    X = np.hstack([np.ones([m,1]), X])
-    
-    #Valroes de prueba de alpha
-    alphas = [1.5, 1.0, 0.5, 0.3, 0.1, 0.03, 0.01]
-    
-    #Valores fijos
-    nIt = 800
-    
-    
-    for alpha in alphas:
-        Theta = np.zeros(n+1)
-        costes = []
-        for i in range(nIt):
-            costes.append(coste(X, Y, Theta, m))
-            Theta = actualizaTheta(X, Y, Theta, m, alpha)
-           
-        fig = plt.figure()
-        plt.scatter(range(nIt), costes, c='blue')
-        plt.xlabel("Número de iteraciones")
-        plt.ylabel("Coste")
-        plt.legend()
-        plt.title("Evolución del coste con alpha=" + str(alpha))
-        plt.savefig("regresion_lineal_varias_variables_alpha_" + str(alpha) + ".png")
+    #Parte 2.1
+    evolucionCostesModificandoAlpha(X, Y, m, n)
         
-    
+    #Parte 2.2
+    ecuacionNormal(X, Y, m, n)
 
 
 main()
