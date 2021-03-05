899c7836f4f78783f439861d4fe8c5fb92293db1
Maria José Belda <majo.belda15@gmail.com>
2021-03-05 13:41:23 +0100

    ingles y ordenado

--- Práctica 1/p1_2.py
+++ Práctica 1/p1_2.py
@@ -10,10 +10,17 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-feet = 1650
-nHab = 3
 
-def normalizar(X):
+
+def getExampleValues():
+    feet = 1650
+    nHab = 3
+    return feet, nHab
+
+def load_data(file):
+    return read_csv(file, header=None).to_numpy().astype(float)
+
+def normalize(X):
     mu = []
     sigma = []
     #Para cada atributo (= columna)
@@ -26,90 +33,93 @@ def normalizar(X):
     return X_norm, mu, sigma
 
     
-def coste(X, Y, Theta, m):
+def costFunction(X, Y, Theta, m):
     diferencias = np.subtract(np.dot(X, Theta), Y)
     return 1/(2*m)*(np.sum(np.power(diferencias, 2)))
 
 def calculaGradiente(X, Y, Theta, m):
     return 1/m * np.dot(np.transpose(X), np.subtract(np.dot(X, Theta), Y))
 
-def actualizaTheta(X, Y, Theta, m, alpha):
+def updateTheta(X, Y, Theta, m, alpha):
     return Theta - alpha*calculaGradiente(X, Y, Theta, m)
 
-def evolucionCostesModificandoAlpha(X, Y, m, n):
+def costFunctionvolutionModifyingAlpha(X, Y, m, n):
     #Normalizo X
-    X, mu, sigma = normalizar(X)
+    X, mu, sigma = normalize(X)
     
-    #Añado una columna de 1s a X
+    #Add 1s column to X
     X = np.hstack([np.ones([m,1]), X])
     
-    #Valroes de prueba de alpha
+    #Alphas values
     alphas = [1.5, 1.0, 0.5, 0.3, 0.1, 0.03, 0.01, 0.001, 0.0001]
     
-    #Valores fijos
-    nIt = 800  
+    #Init values
+    loops = 800  
     
     for alpha in alphas:
         Theta = np.zeros(n+1)
-        costes = []
-        for i in range(nIt):
-            costes.append(coste(X, Y, Theta, m))
-            Theta = actualizaTheta(X, Y, Theta, m, alpha)
+        costs = []
+        for i in range(loops):
+            costs.append(costFunction(X, Y, Theta, m))
+            Theta = updateTheta(X, Y, Theta, m, alpha)
         
         if alpha == 0.01:
-            #Nos guardamos el theta optimo de un buen alpha
+            #It's a good alpha, save optimum theta for later prediction example
             Theta_opt = Theta
-        fig = plt.figure()
-        plt.scatter(range(nIt), costes, c='blue')
+        plt.figure()
+        plt.scatter(range(loops), costs, c='blue')
         plt.xlabel("Número de iteraciones")
-        plt.ylabel("Coste")
+        plt.ylabel("costFunction")
         plt.legend()
-        plt.title("Evolución del coste con alpha=" + str(alpha))
+        plt.title("Evolución del costFunction con alpha=" + str(alpha))
         #plt.savefig("regresion_lineal_varias_variables_alpha_" + str(alpha) + ".png")
     
     
-    print("-------------- DESCENSO DE GRADIENTE --------------")
-    print("Theta optimo: " + str(Theta_opt))
+    print("-------------- GRADIENT DESCENDANT --------------")
+    print("Optimum theta: " + str(Theta))
     
+    feet, nHab = getExampleValues()
     x = np.array([[feet, nHab]])
-    #Normalizo x
+    #Normalize x
     x = np.divide(np.subtract(x, mu), sigma)
     x = np.insert(x, 0, 1)
     
     pred = np.dot(x, Theta_opt)
     
-    print("Vivienda de " + str(feet) + " pies cuadrados y " + str(nHab) + " habitaciones")
-    print("Predicción del precio: "+ str(int(pred)) + "$")
+    print("Home:" + str(feet) + " square foot and " + str(nHab) + " rooms")
+    print("Price prediction: "+ str(int(pred)) + "$")
     
-def ecuacionNormal(X, Y, m, n):
-    #Añado una columna de 1s a X
+def normalEcuation(X, Y, m, n):
+    #Add 1s column to X
     X = np.hstack([np.ones([m,1]), X])
     
-    #Calculamos el theta optimo
+    #Compute optimum theta
     Theta = np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(X), X)), np.transpose(X)), Y)
 
-    print("-------------- ECUACION NORMAL --------------")
-    print("Theta optimo: " + str(Theta))
-    #Predecimos el precio de una casa con 1650 pies cuadrados y 3 habitaciones
+    print("-------------- NORMAL EQUATION --------------")
+    print("Optimum theta: " + str(Theta))
+    #Example
+    feet, nHab = getExampleValues()
     x = [1, feet, nHab]
     pred = np.dot(x, Theta)
-    print("Vivienda de " + str(feet) + " pies cuadrados y " + str(nHab) + " habitaciones")
-    print("Predicción del precio: "+ str(int(pred)) + "$")
+    print("Home:" + str(feet) + " square foot and " + str(nHab) + " rooms")
+    print("Price prediction: "+ str(int(pred)) + "$")
     
 def main():
-    #Cargamos los valores de training
-    datos = read_csv("ex1data2.csv", header=None).to_numpy().astype(float)
+    #Load data
+    values = load_data("ex1data2.csv")
     
-    X = datos[:, :-1]
-    Y = datos[:,-1]
+    X = values[:, :-1]
+    Y = values[:,-1]
     
     m, n = np.shape(X)
     
     #Parte 2.1
-    evolucionCostesModificandoAlpha(X, Y, m, n)
+    costFunctionvolutionModifyingAlpha(X, Y, m, n)
         
     #Parte 2.2
-    ecuacionNormal(X, Y, m, n)
+    normalEcuation(X, Y, m, n)
 
 
-main()
+if __name__ == "__main__":
+    main()
